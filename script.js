0cdc43bcf5e3b4dbd398c1fcf639c38c57e86bf0
Graham Turner <turnertgraham@gmail.com>
2016-04-05 23:34:28 -0400

    dynamically sized pixles, created delegated event handler

--- script.js
+++ script.js
@@ -1,12 +1,41 @@
+var gridSquares = 16;
+var $height = 800 / gridSquares;
+var $width = 800 / gridSquares;
+
 function addDivs(num) {
   for (i = 0; i < num; i++) {
     $(".container").append("<div class='pixel'></div>");
   }
 }
 
+function resetPixels() {
+	$(".pixel").each(function() {
+		if($(this).hasClass("hovered")) {
+			$(this).removeClass("hovered");
+		}
+	});
+}
+
+$(document).ready(function() {
+	addDivs((gridSquares * gridSquares));
 
 
-addDivs(256);
-$(".pixel").on("mouseenter", function() {
-  $(this).addClass("hovered");
+	$("#reset").on("click", resetPixels);
+	$("#reset").on("click", function() {
+		gridSquares = prompt("How big is your mind?")
+		console.log(gridSquares); //Everything below this line is under work
+		$(".pixel").each(function() {
+			$(this).remove();
+		});
+		$height = 800 / gridSquares;
+		$width = 800 / gridSquares;
+		addDivs(gridSquares * gridSquares);
+		$(".pixel").each(function() {
+			$(this).css({"height":$height, "width":$width});
+		});
+	});
+
+	$(".container").on("mouseenter", ".pixel", function() {
+	  $(this).addClass("hovered");
+	});
 });
